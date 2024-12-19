59e28e16f22a20c575669d82eb8c4d47354e7a1d
Maxim Kiryakov <71668634+maxbritva@users.noreply.github.com>
2024-12-19 22:07:07 +0300

    added new state: refill state

--- Assets/Scripts/EntryPoint/EntryPoint.cs
+++ Assets/Scripts/EntryPoint/EntryPoint.cs
@@ -2,6 +2,7 @@ using System;
 using Animations;
 using Game.Board;
 using Game.MatchTiles;
+using Game.Tiles;
 using GameStateMachine;
 using UnityEngine;
 using VContainer;
@@ -16,18 +17,20 @@ namespace EntryPoint
         private Grid _grid;
         private IAnimation _animation;
         private MatchFinder _matchFinder;
+        private TilePool _tilePool;
         
         private void Start()
         {
-            _stateMachine = new StateMachine(_gameBoard, _grid, _animation, _matchFinder);
+            _stateMachine = new StateMachine(_gameBoard, _grid, _animation, _matchFinder, _tilePool);
         }
 
 
-        [Inject] private void Construct(Grid grid, IAnimation animation, MatchFinder matchFinder)
+        [Inject] private void Construct(Grid grid, IAnimation animation, MatchFinder matchFinder, TilePool tilePool)
         {
             _grid = grid;
             _animation = animation;
             _matchFinder = matchFinder;
+            _tilePool = tilePool;
         }
     }
 }
--- Assets/Scripts/GameStateMachine/StateMachine.cs
+++ Assets/Scripts/GameStateMachine/StateMachine.cs
@@ -4,6 +4,7 @@ using Animations;
 using Game.Board;
 using Game.GridSystem;
 using Game.MatchTiles;
+using Game.Tiles;
 using GameStateMachine.States;
 
 namespace GameStateMachine
@@ -16,12 +17,14 @@ namespace GameStateMachine
         private Grid _grid;
         private IAnimation _animation;
         private MatchFinder _matchFinder;
+        private TilePool _tilePool;
 
-        public StateMachine(GameBoard gameBoard, Grid grid, IAnimation animation, MatchFinder matchFinder)
+        public StateMachine(GameBoard gameBoard, Grid grid, IAnimation animation, MatchFinder matchFinder, TilePool tilePool)
         {
             _gameBoard = gameBoard;
             _grid = grid;
             _animation = animation;
+            _tilePool = tilePool;
             _matchFinder = matchFinder;
             _states = new List<IState>()
             {
@@ -29,6 +32,7 @@ namespace GameStateMachine
                 new PlayerTurnState(_grid, this, _animation),
                 new SwapTilesState(_grid, this, _animation, _matchFinder),
                 new RemoveTilesState(grid, this, _animation, _matchFinder),
+                new RefillGridState(grid,this,_animation, _matchFinder, _tilePool, _gameBoard.transform)
             };
             _currentState = _states[0];
             _currentState.Enter();
